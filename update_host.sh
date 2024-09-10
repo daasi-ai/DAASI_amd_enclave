3343a0efe3e971983ec50eee8532d9cabfb47367
aiixt <167081479+aiixt@users.noreply.github.com>
2024-09-10 07:40:38 -0700

    modify install.sh

--- update_host.sh
+++ update_host.sh
@@ -2,15 +2,13 @@
 
 set -x
 
-git clone https://github.com/AMDESE/AMDSEV.git && cd AMDSEV
+cd AMDSEV
 git checkout snp-latest
 rm common.sh
 cp ../common_host.sh .
 mv common_host.sh common.sh
-./build.sh qemu 
+./build.sh qemu
 ./build.sh kernel host
 cp kvm.conf /etc/modprobe.d/
 
-cd linux
-./install.sh
-reboot
+dpkg -i linux/linux-image-*.deb
